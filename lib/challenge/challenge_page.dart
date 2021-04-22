9e0d2569a5cffddfc711836009880c656f1e936f
lucassmelloo <lucasmellovieira99@gmail.com>
2021-04-22 10:27:47 -0300

    Aula 2

--- lib/challenge/challenge_page.dart
+++ lib/challenge/challenge_page.dart
@@ -1,3 +1,5 @@
+import 'package:DevQuiz/challenge/widgets/question_indicator/question_indicator_widget.dart';
+import 'package:DevQuiz/challenge/widgets/quiz/quiz_widget.dart';
 import 'package:flutter/material.dart';
 
 class ChallengePage extends StatefulWidget {
@@ -10,6 +12,14 @@ class ChallengePage extends StatefulWidget {
 class _ChallengePageState extends State<ChallengePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(60),
+        child: SafeArea(child: QuestionIndicatorWidget()),
+      ),
+      body: QuizWidget(
+        title: "O que o Flutter faz em sua totalidade",
+      ),
+    );
   }
 }
